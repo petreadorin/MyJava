63909a2f48f6b44abfcc36a353ab5a387db7f2b2
petreadorin <petreadorin91@gmail.com>
2021-01-27 15:05:57 +0200

    update

--- problems/Pythagora.java
+++ problems/Pythagora.java
@@ -7,10 +7,13 @@ public class Pythagora {
     public static void main(String[] args) {
         float hypo = 0.0f;
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter the length of the first side: ");
         float side1 = scanner.nextFloat();
+
         System.out.println("Enter the length of the second side: ");
         float side2 = scanner.nextFloat();
+
         hypo = (float) Math.sqrt((side1 * side1) + (side2 * side2));
         System.out.println(hypo);
 
